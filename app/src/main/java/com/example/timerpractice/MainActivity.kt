c92aba2cb7a41999479f3fb9d877257b57fd1fb4
worldclasscitizen <jk62362@mensakorea.org>
2023-08-13 13:57:17 +0900

    Updated start, end page

--- app/src/main/java/com/example/timerpractice/MainActivity.kt
+++ app/src/main/java/com/example/timerpractice/MainActivity.kt
@@ -4,43 +4,120 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import org.w3c.dom.Text
 import java.lang.Math.abs
 import java.util.Random
 import java.util.Timer
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+    var p_num = 2
+    var k = 1 // index of player.
+    val score_list = mutableListOf<Float>()
+
+    fun start() {
+
+        setContentView(R.layout.activity_start) // load XML file of Main Page
+        val tv_pnum: TextView = findViewById(R.id.tv_pnum)
+        val btn_minus: TextView = findViewById(R.id.btn_minus)
+        val btn_plus: TextView = findViewById(R.id.btn_plus)
+        val btn_start: TextView = findViewById(R.id.btn_start)
+
+        tv_pnum.text = p_num.toString()
+
+        btn_minus.setOnClickListener {
+            p_num--
+            if(p_num == 0) {
+                p_num = 1
+            }
+            tv_pnum.text = p_num.toString()
+        }
+        btn_plus.setOnClickListener {
+            p_num++
+            tv_pnum.text = p_num.toString()
+        }
+        btn_start.setOnClickListener {
+            main()
+        }
+
+    }
+
+
+    fun main() {
+
+        setContentView(R.layout.activity_main) // load XML file
 
         var timerTask: Timer? = null // nullable
-
-        var isRunning: Boolean = false
+        var stage = 1
         var sec: Int = 0
-        val tv: TextView = findViewById(R.id.tv_random)
+
+        // Views
+        val tv: TextView = findViewById(R.id.tv_pnum)
         val tv_t: TextView = findViewById(R.id.tv_timer)
         val tv_d: TextView = findViewById(R.id.tv_diff)
-        val btn: Button = findViewById(R.id.btn_main)
+        val tv_players: TextView = findViewById(R.id.tv_players)
+        val btn: Button = findViewById(R.id.btn_start)
 
         // Set random time when the application is started.
         val random_box = Random()
         val num = random_box.nextInt(1001)
-        tv.text = (num.toFloat()/100).toString()
+
+        tv.text = (num.toFloat()/100).toString() // Show random number on 'tv(tv_random)'
+        btn.text = "Start"
+        tv_players.text = "Player $k"
 
         btn.setOnClickListener {
-            isRunning = !isRunning
-            if(isRunning == true) {
+            stage++ // there is 3 kinds of stage
+            // stage 1 :
+            // stage 2 : time flows, the text of btn would be changed into 'Stop'
+            // stage 3 :
+            if(stage == 2) {
                 timerTask = kotlin.concurrent.timer(period = 10) {
                     sec++
                     runOnUiThread {
                         tv_t.text = (sec.toFloat()/100).toString()
                     }
                 }
-            } else {
+                btn.text = "Stop"
+            } else if(stage == 3) {
                 timerTask?.cancel()
                 val diff = abs(sec - num).toFloat() / 100
+
+                score_list.add(diff)
+
                 tv_d.text = diff.toString()
+                btn.text = "Next Player"
+                stage = 0
+            } else if(stage == 1) {
+                if(k < p_num) {
+                    k++
+                    main()
+                } else {
+                    end()
+                }
             }
         }
+    }
+
+    fun end() {
+        setContentView(R.layout.activity_end)
+
+        val tv_worst: TextView = findViewById(R.id.tv_worst)
+        val tv_wScore: TextView = findViewById(R.id.tv_wScore)
+        val btn_restart: TextView = findViewById(R.id.btn_restart)
+
+        tv_wScore.text = (score_list.maxOrNull()).toString()
+        var index_last = score_list.indexOf(score_list.maxOrNull())
+        tv_worst.text = "Player " + (index_last + 1).toString()
+
+        btn_restart.setOnClickListener {
+            k = 1
+            score_list.clear()
+            start()
+        }
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        start()
     }
 }
